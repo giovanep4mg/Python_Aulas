99fbbb037f7e984f6f7a720b026dbc2695ff5a7c
giovanep4mg <giovaneteodoro79@gmail.com>
2025-03-29 18:38:06 -0300

    atualizando,,salao

---
+++ Aulas_em_formato_py/09_Manipulacao_de_Strings/Manipulacao_de_Strings.py
@@ -0,0 +1,75 @@
+#####        Manipulação de Strings  
+
+##  O que são Strings?
+
+# Strings são sequências de caracteres utilizadas para armazenar e manipular texto em Python. 
+# Elas podem ser definidas com aspas simples (' ') ou duplas (" ").
+
+print("============================== Exemplo de Strings =========================================")
+print("")
+
+# a var "texto" recebe o texto "Olá mundo!"
+texto = "Olá mundo!"
+
+# Exibirá na tela, o que tem dentro da var "texto"
+print(texto) # saída => Olá mundo!
+
+print("")
+print("=========================== Principais Operações com Strings  =================================")
+print("")
+
+print("==============================  Concatenação  ==============================================")
+print("")
+
+# Podemos unir duas ou mais strings utilizando o operador +.
+
+# Variáveis com o nome e sobrenome
+nome = "Carlos"
+sobrenome ="Silva"
+
+# Junta o nome + sobrenome, dando um espaço " "
+nome_completo = nome + " " + sobrenome
+
+# Exibirá na tela a concatenação das Strings
+print("Nome completo: \n ",nome_completo) # saída => # Nome completo: Carlos Silva
+
+
+print("")
+print("===================================  Repetição  ==============================================")
+print("")
+
+# É possível repetir uma string utilizando o operador *.
+
+#
+risada = "ha" * 5
+
+print(risada)
+
+
+
+
+print("")
+print("===============================================================================================")
+print("")
+
+
+
+
+print("")
+print("===============================================================================================")
+print("")
+
+
+
+
+print("")
+print("===============================================================================================")
+print("")
+
+
+
+
+
+print("")
+print("===============================================================================================")
+print("")
